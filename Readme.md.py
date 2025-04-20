8b85f2edd98da358310151fc4aa47b0e09971d34
maryam123601 <maryamharnuholuwapoisiaq@gmail.com>
2025-04-20 12:39:45 -0700

    Create Readme.md.py

---
+++ Readme.md.py
@@ -0,0 +1,23 @@
+# Nigerian Survey Analysis (Python Project)
+
+This project collects survey data from users and analyzes the top problems faced by males and females in Nigeria using Python.
+
+## Features
+- Collects responses including name, gender, age, address, occupation, and problems.
+- Saves responses to a CSV file.
+- Uses matplotlib to visualize the top 3 problems faced by gender.
+
+## Tools Used
+- Python
+- Matplotlib
+- CSV
+
+## How to Run
+1. Run the Python script to collect data.
+2. Analyze the data using the second script to generate a bar chart of common problems.
+
+## Sample Output
+(You can add a screenshot of your graph here later.)
+
+## Status
+Completed and uploaded as part of my data analysis portfolio.
